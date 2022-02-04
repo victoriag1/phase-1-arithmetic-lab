3cc0a84547e60d58749cf0c2a5359c23164e56aa
Victoria Gonzalez <gonzalezevic@gmail.com>
2022-02-03 16:08:59 -0800

    commit tree 393a29d7222bd2e24a91bf7180f31654454d825e

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31
+let num2 = 2
 
-// Write your code here
+let multiply = num1*num2
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 8
+let num4 = 2
+
+let mod = num3/num2
+
+let max = Math.max(5,10,15,20)
